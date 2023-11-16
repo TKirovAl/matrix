89e41094750ca8421d9ba808ca0e59a30ba47758
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-11-16 10:26:27 +0700

    Update body.cpp

--- body.cpp
+++ body.cpp
@@ -1,30 +1,4 @@
-
-#ifndef MATRIX_H
-#define MATRIX_H
-
-#include <vector>
-
-class Matrix {
-private:
-    std::vector<std::vector<double>> data;
-public:
-    Matrix(const std::vector<std::vector<double>>& data);
-    bool EqMatrix(const Matrix& other) const;
-    void SumMatrix(const Matrix& other);
-    void SubMatrix(const Matrix& other);
-    void MulNumber(double number);
-    void MulMatrix(const Matrix& other);
-    double Determinant() const;
-    Matrix Transpose() const;
-    Matrix CalcComplements() const;
-    Matrix InverseMatrix() const;
-};
-
-#endif
-```
-
-***Файл matrix.cpp***
-```c++
+#include <iostream>
 #include "matrix.h"
 
 Matrix::Matrix(const std::vector<std::vector<double>>& data) : data(data) {}
