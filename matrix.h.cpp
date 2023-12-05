48ce454dd20489c0fde3ce9ecb11303d652dd106
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-12-05 13:44:26 +0700

    Update matrix.h.cpp

--- matrix.h.cpp
+++ matrix.h.cpp
@@ -5,18 +5,26 @@
 
 class Matrix {
 private:
-    int data, cols_;
+    int data, cols_, rows_;
     double** matrix_;
     double** AllocateMatrix(int rows, int cols);
     void Fill();
     
 public:
-    Matrix() noexcept;
+    Matrix();
     Matrix(int rows, int cols);
     int getRows();
     int getCols();
     void OutputMatrix() noexcept;
     void Fill(double** matrix_);
+    bool EqMatrix(const Matrix& other) const;
+    void OutputMatrix() const;
+    void SubMatrix(const Matrix& other);
+    void SumMatrix(const Matrix& other);
+    void MulMatrix(const Matrix& other);
+    Matrix Transpose() const;
+    void MulNumber(double number);
+    double Determinant() const;
 };
 
 #endif
